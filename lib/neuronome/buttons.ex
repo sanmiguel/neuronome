6771c5752cc97a01b0def4cfd1234f04bd422f5c
sanmiguel <michael.coles@gmail.com>
2017-07-02 13:58:39 +0200

    Buttons!
    Presses are now detected by scan_keys
    Still TODO: debounce, edge detection etc

--- lib/neuronome/buttons.ex
+++ lib/neuronome/buttons.ex
@@ -1,8 +1,21 @@
 defmodule Neuronome.Buttons do
   use GenServer
+  use Bitwise
 
   alias ElixirALE.I2C
 
+  @gpioa <<0x12>>
+  @gpiob <<0x13>>
+  @iodira <<0x00>>
+  @iodirb <<0x01>>
+  @iocon <<0x0A>>
+  @gppua <<0x0C>>
+  @gppub <<0x0D>> # This might not be necessary because serial write mode
+
+  @holdtime 500
+  @max_active 10
+  @closed true
+  @open false
 
   def start_link() do
     GenServer.start_link(Neuronome.Buttons, [])
@@ -11,18 +24,92 @@ defmodule Neuronome.Buttons do
   def init(args \\ []) do
     [ dev ] = I2C.device_names()
     {:ok, pid} = I2C.start_link(dev, 0x20)
-    # PortA is Col pins
-    I2C.write(pid, <<0x00, 0xFF>>) # Set PortA to outputs
-    # PortB is Row pins
-    I2C.write(pid, <<0x01, 0x00>>) # Set PortB to inputs
-    # Set inputs to pullup-resistor
-    I2C.write(pid, <<0x0D, 0xFF>>)
 
-    # Set all Columns HIGH
-    I2C.write(pid, <<0x12, 0xFF>>)
+    # Set IOCON: Slew rate disabled # Maybe we don't want this?
+    I2C.write(pid, @iocon <> <<0x00>>)
+    I2C.write(pid, @gppua <> <<255, 255>>) # Set all pullups everywhere
+    I2C.write(pid, @iodira <> <<255, 255>>) # Set all GPIO inputs
+    I2C.write(pid, @gpioa <> <<255, 255>>) # Make output latch agree with pulled-up pins
+    # TODO Wtf does this mean see 
+    # https://github.com/joeyoung/arduino_keypads/blob/master/Keypad_MC17/Keypad_MC17.cpp#L112
 
-    ## TODO Debounce
-    {:ok, pid}
+    rows = :lists.seq(0, 7) # @gpioa
+    cols = :lists.seq(8, 15) # @gpiob
+
+    # pin_state tracks the :high/:low state of each pin (when it's an output)
+    pin_state = pin_state_set(pid)
+    # iodir_state tracks the IO dir of each pin
+    iodir_state = 0xFFFF
+    # NB We keep {pin,iodir}_state as integers because it's easier
+    # for the Bitwise operations later
+    {:ok, {pid, rows, cols, pin_state, iodir_state}}
   end
+
+
+
+  # rows: list of pins (0-idxd) on @gpiob
+  # cols: list of pins (0-idxd) on @gpioa
+  def scan_keys(pid, rows, cols, pin_state, iodir_state) do
+    # Set all row pins to INPUT PULLUP
+    reduce_fun = fn(r, iods) -> pin_mode(pid, r, :input_pullup, iods) end
+    iodir_state = Enum.reduce(rows, iodir_state, reduce_fun)
+
+    read_row = fn(r) -> not pin_read(pid, r) end
+    # For each column pin
+    # - set to OUTPUT
+    # - set LOW
+    # - for each row pin:
+    #  - read pin, invert (active low, invert to high)
+    # - set HIGH
+    # - set to INPUT
+    read_col = fn(c, {ps, iods}) ->
+      iods = pin_mode(pid, c, :output, iods)
+      ps = pin_write(pid, c, :low, ps)
+      row_vals = Enum.map(rows, read_row)
+      ps = pin_write(pid, c, :high, ps)
+      iods = pin_mode(pid, c, :input, iods)
+      {row_vals, {ps, iods}}
+    end
+    {matrix, {pin_state, iodir_state}} = Enum.map_reduce(cols, {pin_state, iodir_state}, read_col)
+    {transpose(matrix), {pin_state, iodir_state}}
+  end
+
+  """
+  key_state: idle | pressed | hold | released
+  mode: :output | :input | :input_pullup
+  """
+
+  defp pin_state_set(pid) do
+    << pin_state :: integer-size(16) >> = I2C.write_read(pid, @gpioa, 2)
+    pin_state
+  end
+
+  defp pin_read(pid, pin) do
+    << values :: integer-size(16) >> = I2C.write_read(pid, @gpioa, 2)
+    mask = 0b1 <<< pin
+    case values &&& mask do
+      ^mask -> true
+      _ -> false
+    end
+  end
+
+  defp pin_write(pid, pin, level, pin_state) do
+    mask = 0b1 <<< pin
+    pin_state = mask_pin_state(level, pin_state, mask)
+    I2C.write(pid, @gpioa <> << pin_state :: integer-size(16) >>)
+    pin_state
+  end
+  defp mask_pin_state(:high, pin_state, mask), do: pin_state ||| mask
+  defp mask_pin_state(_low , pin_state, mask), do: pin_state &&& ~~~mask
+
+  defp pin_mode(pid, pin, mode, iodir_state) do
+    mask = 0b1 <<< pin
+    iodir_state = mask_iodir_state(mode, iodir_state, mask)
+    I2C.write(pid, @iodira <> << iodir_state :: integer-size(16) >>)
+    iodir_state
+  end
+
+  defp mask_iodir_state(:output, iodir_state, mask), do: iodir_state &&& ~~~mask
+  defp mask_iodir_state(_input , iodir_state, mask), do: iodir_state ||| mask
 
 end
