9062af2090fa0078b1b371dae2fce9bef22887cf
sanmiguel <michael.coles@gmail.com>
2017-07-05 18:30:43 +0200

    Remove vestigial idx passing through Key funs

--- lib/neuronome/buttons/key.ex
+++ lib/neuronome/buttons/key.ex
@@ -14,39 +14,42 @@ defmodule Key do
     %Key{ code: code, char: char}
   end
 
-  def next_state(%Key{}=key, idx, button_state) do
+  # Remember: debouncing is taken care of outside of this module
+  # The enclosing module should only call next_state/2 when debouncing is clear
+  def next_state(%Key{}=key, button_state) do
     key = set_unchanged(key)
-    transition(key, idx, millis(), button_state)
+    transition(key, millis(), button_state)
   end
 
   ## TODO Move this somewhere else
   defp millis(), do: :os.system_time / 1_000_000 |> trunc
 
-  defp transition(%Key{state: :idle}=key, idx, time, @closed) do
-    transition_to(%{key | hold_timer: time}, idx, :pressed)
+  defp transition(%Key{state: :idle}=key, time, @closed) do
+    transition_to(%{key | hold_timer: time}, :pressed)
   end
   # TODO NB This uses a timer per key, not a global timer as per the original
-  defp transition(%Key{state: :pressed, hold_timer: time_was}=key, idx, time_now, _)
+  defp transition(%Key{state: :pressed, hold_timer: time_was}=key, time_now, _)
   when (time_now - time_was)> @holdtime do
-    transition_to(key, idx, :hold)
+    transition_to(key, :hold)
   end
-  defp transition(%Key{state: :pressed}=key, idx, _, @open) do
-    transition_to(key, idx, :released)
+  defp transition(%Key{state: :pressed}=key, _, @open) do
+    transition_to(key, :released)
   end
-  defp transition(%Key{state: :hold}=key, idx, _, @open) do
-    transition_to(key, idx, :released)
+  defp transition(%Key{state: :hold}=key, _, @open) do
+    transition_to(key, :released)
   end
-  defp transition(%Key{state: :released}=key, idx, _, button_state) do
-    transition_to(key, idx, :idle)
+  defp transition(%Key{state: :released}=key, _, button_state) do
+    transition_to(key, :idle)
   end
-  defp transition(key, _idx, _, _button_state), do: key
+  defp transition(key, _, _button_state), do: key
 
   def set_unchanged(%Key{}=key), do: %{key | state_changed: false}
 
-  defp transition_to(%Key{}=key, _idx, next_state) do
+  defp transition_to(%Key{}=key, next_state) do
     # TODO Trigger event listeners (char) / send messages / whatever
     %{key | state: next_state, state_changed: true}
   end
+
   defp pressed(cols, col, row) do
     :lists.nth(row+1, :lists.nth(col+1, cols))
   end
