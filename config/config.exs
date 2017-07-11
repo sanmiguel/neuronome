c25c827cf47e2ff4399b9510f90e685a9152bbfd
sanmiguel <michael.coles@gmail.com>
2017-07-11 16:40:17 +0200

    Add config for nerves_network defaults, nerves_firmware_ssh keys placeholder

--- config/config.exs
+++ config/config.exs
@@ -5,6 +5,16 @@
 # is restricted to this project.
 use Mix.Config
 
+config :nerves_network, :default,
+  wlan0: [
+    ssid: System.get_env("NERVES_WIFI_SSID"),
+    key_mgmt: :"WPA-PSK",
+    psk: System.get_env("NERVES_WIFI_PSK")
+  ],
+  eth0: [
+    ipv4_address_method: :dhcp
+  ]
+
 config :nerves_network,
   regulatory_domain: :"PL"
 
@@ -14,6 +24,10 @@ config :neuronome, :wlan0,
   key_mgmt: :"WPA-PSK",
   psk: System.get_env("NERVES_WIFI_PSK")
 
+# SSH Keys for nerves_firmware_ssh
+config :nerves_firmware_ssh,
+  authorized_keys: [
+  ]
 # Customize the firmware. Uncomment all or parts of the following
 # to add files to the root filesystem or modify the firmware
 # archive.
